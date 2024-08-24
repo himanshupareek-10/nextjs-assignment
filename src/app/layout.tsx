4972d5331a8c1674df2e67e989d2dda4fadca3d5
Apple <apple@Apples-MacBook-Air.local>
2024-08-24 16:52:44 +0530

    test commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,13 +24,13 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <LoginProvider>
-        <AuthProvider>
-          <Header/>
-          {children}
-          <Footer/>
-        </AuthProvider>
+          <AuthProvider>
+            <Header />
+            {children}
+            <Footer />
+          </AuthProvider>
         </LoginProvider>
-        </body>
+      </body>
     </html>
   );
 }
